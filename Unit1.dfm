ca94886b13f691c070fac09aff5f868bc0bd9a22
m.pryanikov <m.pryanikov@rapida.ru>
2020-01-10 15:25:33 +0300

    А теперь попробуем выполнить несколько задач параллельно. Для этого нужно создать массив задач, запустить задачи и ждать когда они выполнятся. Для ожидания есть две статические функции класса TTask: WaitForAll (ожидать выполнения всех задач) и WaitForAny (ожидать выполнения хотя бы одной из задач)

--- Unit1.dfm
+++ Unit1.dfm
@@ -14,12 +14,21 @@ object Form1: TForm1
   PixelsPerInch = 96
   TextHeight = 13
   object Button1: TButton
-    Left = 288
-    Top = 216
+    Left = 8
+    Top = 16
     Width = 75
     Height = 25
     Caption = 'Button1'
     TabOrder = 0
     OnClick = Button1Click
+  end
+  object Button2: TButton
+    Left = 89
+    Top = 16
+    Width = 75
+    Height = 25
+    Caption = 'Button2'
+    TabOrder = 1
+    OnClick = Button2Click
   end
 end
--- Unit1.pas
+++ Unit1.pas
@@ -4,12 +4,14 @@ interface
 
 uses
   Winapi.Windows, Winapi.Messages, System.SysUtils, System.Variants, System.Classes, Vcl.Graphics,
-  Vcl.Controls, Vcl.Forms, Vcl.Dialogs, Vcl.StdCtrls, System.Threading;
+  Vcl.Controls, Vcl.Forms, Vcl.Dialogs, Vcl.StdCtrls, System.Threading, System.SyncObjs;
 
 type
   TForm1 = class(TForm)
     Button1: TButton;
+    Button2: TButton;
     procedure Button1Click(Sender: TObject);
+    procedure Button2Click(Sender: TObject);
   private
     { Private declarations }
   public
@@ -37,6 +39,56 @@ begin
       end);
    //Запускаем задачу.
    task.Start;
+end;
+
+procedure TForm1.Button2Click(Sender: TObject);
+var
+   tasks: array of ITask;
+   task: ITask;
+   value: integer;
+
+   procedure CreateTasks;
+   begin
+      value := 0;
+      tasks := [
+         TTask.Create(procedure()
+            begin
+               //Выполняем задачу 5 секунд.
+               Sleep(5000);
+               //Добавляем к результату 5000.
+               TInterlocked.Add(value, 5000);
+            end
+         ),
+         TTask.Create(procedure()
+            begin
+               //Выполняем задачу 3 секунды.
+               Sleep(3000);
+               //Добавляем к результату 3000.
+               TInterlocked.Add(value, 3000);
+            end
+         )
+      ];
+   end;
+
+begin
+   //Создаём задачи и инициализируем переменную value.
+   CreateTasks;
+   //Запускаем все задачи в массиве.
+   for task in tasks do
+      task.Start;
+   //Ждём выполнение всех задач.
+   TTask.WaitForAll(tasks);
+   //Результат будет 8000.
+   ShowMessage('Все задания выполнены. Результат: ' + IntToStr(value));
+   //Создаём задачи и инициализируем переменную value.
+   CreateTasks;
+   //Запускаем все задачи в массиве.
+   for task in tasks do
+      task.Start;
+   //Ждём выполнение любой из задач.
+   TTask.WaitForAny(tasks);
+   //Результат будет 3000.
+   ShowMessage('Все задания выполнены. Результат: ' + IntToStr(value));
 end;
 
 end.
